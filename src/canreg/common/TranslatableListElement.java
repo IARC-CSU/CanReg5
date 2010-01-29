a5769944c42ad088a50d141006ef4012bca6e7e0
Morten Johannes Ervik <ervikm@iarc.fr>
2010-01-29 15:55:17 +0100

    TranslatableListElement: Added equals().

--- src/canreg/common/TranslatableListElement.java
+++ src/canreg/common/TranslatableListElement.java
@@ -9,17 +9,35 @@ public class TranslatableListElement {
     String translatedName;
     String originalName;
 
-    public TranslatableListElement(String originalName, String translatedName){
+    public TranslatableListElement(String originalName, String translatedName) {
         this.originalName = originalName;
         this.translatedName = translatedName;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return translatedName;
     }
 
-    public String getOriginalName(){
+    @Override
+    public boolean equals(Object o) {
+        boolean equal = false;
+        if (o instanceof TranslatableListElement) {
+            equal = this.hashCode() == o.hashCode();
+        } else {
+            super.equals(o);
+        }
+        return equal;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 47 * hash + (this.originalName != null ? this.originalName.hashCode() : 0);
+        return hash;
+    }
+
+    public String getOriginalName() {
         return originalName;
     }
 }
