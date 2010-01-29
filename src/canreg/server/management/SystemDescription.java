ef1a3ffc5ac0b9d0705d44ad3c54151972e105b3
Morten Johannes Ervik <ervikm@iarc.fr>
2010-01-29 15:57:26 +0100

    SystemDescription: Changes to accomodate the changes in DatabaseListElements, added setters to change the database's doc.

--- src/canreg/server/management/SystemDescription.java
+++ src/canreg/server/management/SystemDescription.java
@@ -2,13 +2,16 @@ package canreg.server.management;
 
 import canreg.common.DatabaseDictionaryListElement;
 import canreg.common.DatabaseGroupsListElement;
+import canreg.common.DatabaseIndexesListElement;
 import canreg.common.DatabaseVariablesListElement;
 import canreg.common.Globals;
 import canreg.common.Tools;
 import canreg.server.database.QueryGenerator;
 import com.sun.org.apache.xerces.internal.parsers.DOMParser;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.w3c.dom.*;
@@ -24,17 +27,18 @@ public class SystemDescription {
     private static boolean debug = Globals.DEBUG;
     private Document doc;
     private DOMParser parser;
-    private String fileName;
     private String namespace = "ns3:";
-
-    ;
+    private String[] canreg4dateFormats = new String[]{"dd/mm/yyyy", "mm/dd/yyyy", "budhist", "yyyy/mm/dd"};
+    private DatabaseDictionaryListElement[] dictionaryListElements;
+    private DatabaseGroupsListElement[] groupListElements;
+    private DatabaseVariablesListElement[] variableListElements;
+    private DatabaseIndexesListElement[] indexListElements;
 
     /**
      * 
      * @param fileName
      */
     public SystemDescription(String fileName) {
-        this.fileName = fileName;
         try {
             parser = new DOMParser();
 
@@ -46,6 +50,7 @@ public class SystemDescription {
                 //canreg.server.xml.Tools.flushXMLout(doc);
                 debugOut(QueryGenerator.strCreateVariableTable("Patient", doc));
                 debugOut(QueryGenerator.strCreateVariableTable("Tumour", doc));
+                debugOut(QueryGenerator.strCreateVariableTable("Source", doc));
                 debugOut(QueryGenerator.strCreateDictionaryTable(doc));
                 debugOut(QueryGenerator.strCreateTablesOfDictionaries(doc));
             }
@@ -65,6 +70,10 @@ public class SystemDescription {
     public void setSystemDescriptionXML(String fileName) throws SAXException, IOException {
         parser.parse(new InputSource(new FileInputStream(fileName)));
         doc = parser.getDocument();
+        groupListElements = Tools.getGroupsListElements(doc, namespace);
+        dictionaryListElements = Tools.getDictionaryListElements(doc, namespace);
+        variableListElements = Tools.getVariableListElements(doc, Globals.NAMESPACE, getDictionaryMap(), getGroupMap());
+        indexListElements = Tools.getIndexesListElements(doc, namespace, getVariablesMap());
     }
 
     /**
@@ -169,15 +178,19 @@ public class SystemDescription {
     }
 
     public DatabaseVariablesListElement[] getDatabaseVariableListElements() {
-        return Tools.getVariableListElements(doc, Globals.NAMESPACE);
+        return variableListElements;
     }
 
     public DatabaseDictionaryListElement[] getDatabaseDictionaryListElements() {
-        return Tools.getDictionaryListElements(doc, namespace);
+        return dictionaryListElements;
     }
 
     public DatabaseGroupsListElement[] getDatabaseGroupsListElements() {
-        return Tools.getGroupsListElements(doc, namespace);
+        return groupListElements;
+    }
+
+    public DatabaseIndexesListElement[] getDatabaseIndexesListElements() {
+        return indexListElements;
     }
 
     /**
@@ -195,16 +208,215 @@ public class SystemDescription {
         return element;
     }
 
-    /**
-     * 
-     * @param var
-     */
-    public void addVariable(Variable var) {
+    public String getDateFormat() {
+        Element element = null;
+        String dateFormat = null;
+        if (doc != null) {
+            NodeList nl = doc.getElementsByTagName(namespace + "date_format");
+            if (nl != null) {
+                element = (Element) nl.item(0);
+                dateFormat = element.getTextContent();
+                if (dateFormat.trim().length() == 1) {
+                    try {
+                        int i = Integer.parseInt(dateFormat);
+                        dateFormat = canreg4dateFormats[i];
+                    } catch (NumberFormatException nfe) {
+                        // do nothing
+                    }
+                }
+            }
+        }
+        return dateFormat;
+    }
+
+    public String getTextContentFromElement(String elementName) {
+        Element element = null;
+        String elementContent = null;
+        if (doc != null) {
+            NodeList nl = doc.getElementsByTagName(namespace + elementName);
+            if (nl != null) {
+                element = (Element) nl.item(0);
+                elementContent = element.getTextContent();
+            }
+        }
+        return elementContent;
+    }
+
+    public void setTextContentForExistingElement(String elementName, String content) {
+        if (doc != null) {
+            NodeList nl = doc.getElementsByTagName(namespace + "elementName");
+            if (nl != null) {
+                Element element = (Element) nl.item(0);
+                element.setTextContent(content);
+            }
+        }
+    }
+
+    public void setVariables(DatabaseVariablesListElement[] variables) {
+        Element variablesParentElement = (Element) doc.getElementsByTagName(namespace + "variables").item(0);
+        if (variablesParentElement == null) {
+            variablesParentElement = doc.createElement(namespace + "variables");
+            doc.appendChild(variablesParentElement);
+        }
+        //  debugOut(i+ " " + variableElement.getElementsByTagName(namespace + "short_name").item(0).getTextContent());
+        while (variablesParentElement.hasChildNodes()) {
+            variablesParentElement.removeChild(variablesParentElement.getLastChild());
+        }
+        for (DatabaseVariablesListElement variable : variables) {
+            Element element = createVariable(variable);
+            variablesParentElement.appendChild(element);
+        }
+    }
+
+    public void setDictionaries(DatabaseDictionaryListElement[] dictionaries) {
+        Element parentElement = (Element) doc.getElementsByTagName(namespace + "dictionaries").item(0);
+        if (parentElement == null) {
+            parentElement = doc.createElement(namespace + "dictionaries");
+            doc.appendChild(parentElement);
+        }
+        //  debugOut(i+ " " + variableElement.getElementsByTagName(namespace + "short_name").item(0).getTextContent());
+        while (parentElement.hasChildNodes()) {
+            parentElement.removeChild(parentElement.getLastChild());
+        }
+        for (DatabaseDictionaryListElement dictionary : dictionaries) {
+            Element element = createDictionary(dictionary);
+            parentElement.appendChild(element);
+        }
+    }
+
+    public void setGroups(DatabaseGroupsListElement[] groups) {
+        Element parentElement = (Element) doc.getElementsByTagName(namespace + "groups").item(0);
+        if (parentElement == null) {
+            parentElement = doc.createElement(namespace + "groups");
+            doc.appendChild(parentElement);
+        }
+        //  debugOut(i+ " " + variableElement.getElementsByTagName(namespace + "short_name").item(0).getTextContent());
+        while (parentElement.hasChildNodes()) {
+            parentElement.removeChild(parentElement.getLastChild());
+        }
+        for (DatabaseGroupsListElement group : groups) {
+            Element element = createGroup(group);
+            parentElement.appendChild(element);
+        }
+    }
+
+    private Element createVariable(DatabaseVariablesListElement variable) {
+        Element element = doc.createElement(namespace + "variable");
+        element.appendChild(createElement(namespace + "variable_id", "" + variable.getVariableID()));
+        element.appendChild(createElement(namespace + "full_name", variable.getFullName()));
+        element.appendChild(createElement(namespace + "short_name", variable.getShortName()));
+        element.appendChild(createElement(namespace + "english_name", variable.getEnglishName()));
+        element.appendChild(createElement(namespace + "group_id", "" + variable.getGroupID()));
+        element.appendChild(createElement(namespace + "fill_in_status", variable.getFillInStatus()));
+        element.appendChild(createElement(namespace + "multiple_primary_copy", variable.getMultiplePrimaryCopy()));
+        element.appendChild(createElement(namespace + "variable_type", variable.getVariableType()));
+        // Varb Type  (0 number, 1 alpha, 2 date, 3 dict, 4 asian text)
+        if (variable.getVariableType().equalsIgnoreCase(Globals.VARIABLE_TYPE_ALPHA_NAME) || variable.getVariableType().equalsIgnoreCase(Globals.VARIABLE_TYPE_NUMBER_NAME) || variable.getVariableType().equalsIgnoreCase(Globals.VARIABLE_TYPE_ASIAN_TEXT_NAME)) {
+            element.appendChild(createElement(namespace + "variable_length", "" + variable.getVariableLength()));
+        } else if (variable.getVariableType().equalsIgnoreCase(Globals.VARIABLE_TYPE_DATE_NAME)) {
+            element.appendChild(createElement(namespace + "variable_length", "" + Globals.DATE_FORMAT_STRING.length()));
+        } else if (variable.getVariableType().equalsIgnoreCase(Globals.VARIABLE_TYPE_DICTIONARY_NAME)) {
+            element.appendChild(createElement(namespace + "use_dictionary", "" + variable.getUseDictionary()));
+            // (0 Simple, 1 Compound)
+        } else {
+            debugOut("Invalid variable description...");
+        }
+        // Place variable in the right table
+        element.appendChild(createElement(namespace + "table", variable.getTable()));
+        // variableToTableMap.put(shortName, table);
+        if (variable.getStandardVariableName() != null) {
+            element.appendChild(createElement(namespace + "standard_variable_name", variable.getStandardVariableName()));
+        }
+        element.appendChild(createElement(namespace + "variable_X_pos", "" + variable.getXPos()));
+        element.appendChild(createElement(namespace + "variable_Y_pos", "" + variable.getYPos()));
+        return element;
+    }
+
+    private Element createDictionary(DatabaseDictionaryListElement dictionary) {
+        Element element = doc.createElement(namespace + "dictionary");
+        element.appendChild(createElement(namespace + "dictionary_id", "" + dictionary.getDictionaryID()));
+        element.appendChild(createElement(namespace + "name", dictionary.getName()));
+        element.appendChild(createElement(namespace + "font", dictionary.getFont()));
+        element.appendChild(createElement(namespace + "type", dictionary.getType()));
+        element.appendChild(createElement(namespace + "code_length", "" + dictionary.getCodeLength()));
+        element.appendChild(createElement(namespace + "category_description_length", "" + dictionary.getCategoryDescriptionLength()));
+        element.appendChild(createElement(namespace + "full_dictionary_code_length", "" + dictionary.getFullDictionaryCodeLength()));
+        element.appendChild(createElement(namespace + "full_dictionary_description_length", "" + dictionary.getFullDictionaryCategoryDescriptionLength()));
+        if (dictionary.isLocked()) {
+            element.appendChild(createElement(namespace + "locked", "true"));
+        }
+        return element;
+    }
+
+    private Element createElement(String variableName, String value) {
+        Element childElement = doc.createElement(variableName);
+        childElement.appendChild(doc.createTextNode(value));
+        return childElement;
     }
 
     private static void debugOut(String msg) {
         if (debug) {
             Logger.getLogger(SystemDescription.class.getName()).log(Level.INFO, msg);
+        }
+    }
+
+    public void saveSystemDescriptionXML(String path) {
+        File file = new File(Globals.CANREG_SERVER_SYSTEM_CONFIG_FOLDER); // Check to see it the canreg system folder exists
+        if (!file.exists()) {
+            file.mkdirs(); // create it if necessary
+        }
+        canreg.server.xml.Tools.writeXmlFile(doc, path);
+    }
+
+    private TreeMap<String, DatabaseDictionaryListElement> getDictionaryMap() {
+        TreeMap<String, DatabaseDictionaryListElement> dictionaryMap = new TreeMap<String, DatabaseDictionaryListElement>();
+        for (DatabaseDictionaryListElement dictionary : dictionaryListElements) {
+            dictionaryMap.put(dictionary.getName(), dictionary);
+        }
+        return dictionaryMap;
+    }
+
+    private TreeMap<String, DatabaseGroupsListElement> getGroupMap() {
+        TreeMap<String, DatabaseGroupsListElement> groupsMap = new TreeMap<String, DatabaseGroupsListElement>();
+        for (DatabaseGroupsListElement group : groupListElements) {
+            groupsMap.put(group.getGroupIndex() + "", group);
+        }
+        return groupsMap;
+    }
+
+    private TreeMap<String, DatabaseVariablesListElement> getVariablesMap() {
+        TreeMap<String, DatabaseVariablesListElement> variablesMap = new TreeMap<String, DatabaseVariablesListElement>();
+        for (DatabaseVariablesListElement variable : variableListElements) {
+            variablesMap.put(variable.getDatabaseVariableName().toUpperCase(), variable);
+        }
+        return variablesMap;
+    }
+
+    private Element createGroup(DatabaseGroupsListElement group) {
+        Element element = doc.createElement(namespace + "group");
+        element.appendChild(createElement(namespace + "group_id", "" + group.getGroupIndex()));
+        element.appendChild(createElement(namespace + "name", group.getGroupName()));
+        element.appendChild(createElement(namespace + "group_pos", "" + group.getGroupPosition()));
+        return element;
+    }
+
+    public void setRegistryCode(String registryCode) {
+        if (doc != null) {
+            NodeList nl = doc.getElementsByTagName(namespace + "registry_code");
+            if (nl != null) {
+                Element element = (Element) nl.item(0);
+                element.setTextContent(registryCode);
+            }
+        }
+    }
+
+    public void setRegionCode(int regionCode) {
+        if (doc != null) {
+            NodeList nl = doc.getElementsByTagName(namespace + "region_code");
+            if (nl != null) {
+                Element element = (Element) nl.item(0);
+                element.setTextContent(regionCode + "");
+            }
         }
     }
 }
