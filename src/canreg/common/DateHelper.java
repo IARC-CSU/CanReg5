5754e6d248021fd8376919f1aecde95ce8a8e76c
Morten Johannes Ervik <ervikm@iarc.fr>
2010-02-23 10:24:34 +0100

    DateHelper: difference in dates in days calculated properly.

--- src/canreg/common/DateHelper.java
+++ src/canreg/common/DateHelper.java
@@ -177,7 +177,7 @@ public class DateHelper {
     public static long daysBetween(Calendar startDate, Calendar endDate) {
         Calendar date = (Calendar) startDate.clone();
         long daysBetween = 0;
-        while (date.before(endDate)) {
+        while (date.before(endDate) || date.equals(endDate)) {
             date.add(Calendar.DAY_OF_MONTH, 1);
             daysBetween++;
         }
