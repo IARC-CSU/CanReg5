79e87fcc86a67664ebdaf40ec16a5277b62097ef
Morten Johannes Ervik <ervikm@iarc.fr>
2010-02-25 11:15:36 +0100

    AutoFillHelper: auto age calculation implemented...

--- src/canreg/client/gui/dataentry/AutoFillHelper.java
+++ src/canreg/client/gui/dataentry/AutoFillHelper.java
@@ -1,10 +1,15 @@
 package canreg.client.gui.dataentry;
 
 import canreg.common.DatabaseVariablesListElement;
+import canreg.common.DateHelper;
 import canreg.common.GlobalToolBox;
 import canreg.common.Globals;
 import canreg.server.database.DatabaseRecord;
+import java.text.ParseException;
+import java.util.Calendar;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,23 +20,53 @@ class AutoFillHelper {
     private GlobalToolBox globalToolBox;
 
     public void autoFill(LinkedList<DatabaseVariablesListElement> autoFillList, DatabaseRecord sourceOfActionDatabaseRecord, DatabaseRecord otherDatabaseRecord, RecordEditorPanel recordEditorPanel) {
+
         for (DatabaseVariablesListElement dvle : autoFillList) {
             String standardVariableName = dvle.getStandardVariableName();
             if (standardVariableName == null) {
                 // recordEditorPanel.setVariable(dvle, "");
             } else if (Globals.StandardVariableNames.Age.toString().equalsIgnoreCase(standardVariableName)) {
-                String code = "";
-                // Object unknownCodeObject = dvle.getUnknownCode();
-                // if (unknownCodeObject != null) {
-                //    code = unknownCodeObject.toString();
-                // } else {
-                // old school unknown code
-                // TODO: Calculate age in autofill!
-                    for (int i = 0; i < dvle.getVariableLength(); i++) {
-                        code += "9";
+                Object code = null;
+                DatabaseVariablesListElement incidenceDateVariableElement = globalToolBox.translateStandardVariableNameToDatabaseListElement(Globals.StandardVariableNames.IncidenceDate.toString());
+                Object incValue;
+                DatabaseVariablesListElement birthDateVariableElement = globalToolBox.translateStandardVariableNameToDatabaseListElement(Globals.StandardVariableNames.BirthDate.toString());
+                Object birthValue;
+
+                // try to get the variable from the source
+                incValue = sourceOfActionDatabaseRecord.getVariable(incidenceDateVariableElement.getDatabaseVariableName());
+                if (incValue == null) {
+                    incValue = otherDatabaseRecord.getVariable(incidenceDateVariableElement.getDatabaseVariableName());
+                }
+
+                birthValue = sourceOfActionDatabaseRecord.getVariable(birthDateVariableElement.getDatabaseVariableName());
+                if (birthValue == null) {
+                    birthValue = otherDatabaseRecord.getVariable(birthDateVariableElement.getDatabaseVariableName());
+                }
+
+                if (birthValue != null && incValue != null) {
+                    Calendar birthDate;
+                    Calendar incDate;
+                    try {
+                        birthDate = DateHelper.parseDateStringToGregorianCalendarCanReg(birthValue.toString(), Globals.DATE_FORMAT_STRING);
+                        incDate = DateHelper.parseDateStringToGregorianCalendarCanReg(incValue.toString(), Globals.DATE_FORMAT_STRING);
+                        int age = (int) DateHelper.yearsBetween(birthDate, incDate);
+                        code = age;
+                    } catch (ParseException ex) {
+                        Logger.getLogger(AutoFillHelper.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalArgumentException ex) {
+                        Logger.getLogger(AutoFillHelper.class.getName()).log(Level.SEVERE, null, ex);
                     }
-                // }
-                recordEditorPanel.setVariable(dvle, code);
+                } else {
+                    code = dvle.getUnknownCode();
+                    if (code == null) {
+                        String codeString = "";
+                        for (int i = 0; i < dvle.getVariableLength(); i++) {
+                            codeString += "9";
+                        }
+                        code = codeString;
+                    }
+                }
+                recordEditorPanel.setVariable(dvle, code + "");
             } else if (Globals.StandardVariableNames.Behaviour.toString().equalsIgnoreCase(standardVariableName)) {
                 recordEditorPanel.setVariable(dvle, "Behaviour to be calculated");
                 DatabaseVariablesListElement morphologyVariableElement = globalToolBox.translateStandardVariableNameToDatabaseListElement(Globals.StandardVariableNames.Morphology.toString());
