ce659b37dac9055372fd0beb5a3fb73d87679d4b
Morten Johannes Ervik <ervikm@iarc.fr>
2011-01-05 11:46:32 +0100

    Fixed a bug when the standard variable was a string of 0 lenght. Tidied some code.

--- src/canreg/common/GlobalToolBox.java
+++ src/canreg/common/GlobalToolBox.java
@@ -14,18 +14,19 @@ import org.w3c.dom.Document;
  * This contains all non-static information needed by the CanReg program.
  */
 public class GlobalToolBox {
+
     private Document doc;
     private Map<String, DatabaseVariablesListElement> standardVariableNameToDatabaseVariableListElementMap;
     private Map<Integer, DatabaseGroupsListElement> groupIDToDatabaseGroupListElementMap;
     private DatabaseVariablesListElement[] databaseVariablesListElements;
     private Charset standardCharSet;
-   
+
     /**
      * 
      * @param doc
      */
     public GlobalToolBox(Document doc) {
-        this.doc = doc;        
+        this.doc = doc;
         groupIDToDatabaseGroupListElementMap = buildGroupMap(Tools.getGroupsListElements(doc, Globals.NAMESPACE));
         databaseVariablesListElements = Tools.getVariableListElements(doc, Globals.NAMESPACE);
         standardVariableNameToDatabaseVariableListElementMap = buildVariablesMap(databaseVariablesListElements);
@@ -47,59 +48,57 @@ public class GlobalToolBox {
     public DatabaseVariablesListElement[] getVariables() {
         return databaseVariablesListElements;
     }
-    
+
     /**
      * 
      * @param standardVariableName
      * @return
      */
-    public DatabaseVariablesListElement translateStandardVariableNameToDatabaseListElement(String standardVariableName){
+    public DatabaseVariablesListElement translateStandardVariableNameToDatabaseListElement(String standardVariableName) {
         return standardVariableNameToDatabaseVariableListElementMap.get(standardVariableName);
     }
-    
+
     /**
      * 
      * @return
      */
-    public LinkedList<DatabaseVariablesListElement> getStandardVariables(){
+    public LinkedList<DatabaseVariablesListElement> getStandardVariables() {
         Iterator<DatabaseVariablesListElement> it = standardVariableNameToDatabaseVariableListElementMap.values().iterator();
         DatabaseVariablesListElement dvle;
         LinkedList<DatabaseVariablesListElement> variables = new LinkedList<DatabaseVariablesListElement>();
-        while (it.hasNext()){
+        while (it.hasNext()) {
             dvle = it.next();
             variables.add(dvle);
         }
         return variables;
     }
-    
-    
 
     /**
      * 
      * @param groupID
      * @return
      */
-    public DatabaseGroupsListElement translateGroupIDToDatabaseGroupListElement(Integer groupID){
+    public DatabaseGroupsListElement translateGroupIDToDatabaseGroupListElement(Integer groupID) {
         return groupIDToDatabaseGroupListElementMap.get(groupID);
     }
-    
-    private static Map<Integer, DatabaseGroupsListElement> buildGroupMap(DatabaseGroupsListElement[] listElements){
+
+    private static Map<Integer, DatabaseGroupsListElement> buildGroupMap(DatabaseGroupsListElement[] listElements) {
         Map map = new LinkedHashMap();
-        for (DatabaseGroupsListElement dvle:listElements){
+        for (DatabaseGroupsListElement dvle : listElements) {
             Integer groupID = dvle.getGroupIndex();
-            if (groupID!=null){
+            if (groupID != null) {
                 map.put(groupID, dvle);
             }
         }
         return map;
     }
-    
-    private static Map<String, DatabaseVariablesListElement> buildVariablesMap(DatabaseVariablesListElement[] listelements){
+
+    private static Map<String, DatabaseVariablesListElement> buildVariablesMap(DatabaseVariablesListElement[] listelements) {
         Map map = new LinkedHashMap();
         // First build the real variables
-        for (DatabaseVariablesListElement dvle:listelements){
+        for (DatabaseVariablesListElement dvle : listelements) {
             String standardVariableName = dvle.getStandardVariableName();
-            if (standardVariableName!=null){
+            if (standardVariableName != null && standardVariableName.length() > 0) {
                 map.put(standardVariableName, dvle);
             }
         }
@@ -107,5 +106,4 @@ public class GlobalToolBox {
 
         return map;
     }
-    
 }
