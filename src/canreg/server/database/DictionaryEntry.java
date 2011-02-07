218a64c5f9917b8390e9c3779dde48a1ee8c8171
Morten Johannes Ervik <ervikm@iarc.fr>
2011-02-07 16:24:48 +0100

    DictionaryEntry can now be added to a tree to be sorted by either code or description.

--- src/canreg/server/database/DictionaryEntry.java
+++ src/canreg/server/database/DictionaryEntry.java
@@ -6,10 +6,12 @@ import java.io.Serializable;
  *
  * @author ervikm
  */
-public class DictionaryEntry implements Serializable {
+public class DictionaryEntry implements Serializable, Comparable {
+
     private int dictionaryID;
     private String code;
     private String description;
+    private boolean sortByCode = false;
 
     /**
      * 
@@ -17,12 +19,12 @@ public class DictionaryEntry implements Serializable {
      * @param code
      * @param description
      */
-    public DictionaryEntry(int dicID, String code, String description){
+    public DictionaryEntry(int dicID, String code, String description) {
         this.dictionaryID = dicID;
         this.code = code;
         this.description = description;
     }
-    
+
     /**
      * 
      * @return
@@ -73,16 +75,21 @@ public class DictionaryEntry implements Serializable {
 
     @Override
     public String toString() {
-        return code +" - "+description;
+        if (sortByCode) {
+            return code + " - " + description;
+        } else {
+            return description + " (" + code + ")";
+        }
     }
-    
+
     @Override
-    public boolean equals(Object o){
-        if (o instanceof DictionaryEntry){
+    public boolean equals(Object o) {
+        if (o instanceof DictionaryEntry) {
             DictionaryEntry de = (DictionaryEntry) o;
             return code.equals(de.getCode());
-        } else 
+        } else {
             return super.equals(o);
+        }
     }
 
     @Override
@@ -90,5 +97,27 @@ public class DictionaryEntry implements Serializable {
         int hash = 7;
         hash = 37 * hash + (this.code != null ? this.code.hashCode() : 0);
         return hash;
+    }
+
+    public void setSortByCode() {
+        sortByCode = true;
+    }
+
+    public void setSortByDescription() {
+        sortByCode = false;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o.getClass().isInstance(this)) {
+            DictionaryEntry other = (DictionaryEntry) o;
+            if (sortByCode) {
+                return this.getCode().compareTo(other.getCode());
+            } else {
+                return this.getDescription().compareTo(other.getDescription());
+            }
+        } else {
+            return -1;
+        }
     }
 }
