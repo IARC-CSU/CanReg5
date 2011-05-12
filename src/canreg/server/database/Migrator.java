e83262fe41df253ea8a473983aecaae0ae15e2a6
Morten Johannes Ervik <ervikm@iarc.fr>
2011-05-12 15:13:09 +0200

    Added code to migrate the database to 5.00.06 - add foreign keys etc.

--- src/canreg/server/database/Migrator.java
+++ src/canreg/server/database/Migrator.java
@@ -4,6 +4,7 @@
  */
 package canreg.server.database;
 
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +13,7 @@ import java.util.logging.Logger;
  * @author ervikm
  */
 public class Migrator {
+
     private String newVersion;
     private CanRegDAO canRegDAO;
 
@@ -20,9 +22,9 @@ public class Migrator {
      * @param newVersion
      * @param canRegDAO
      */
-    public Migrator(String newVersion, CanRegDAO canRegDAO){
+    public Migrator(String newVersion, CanRegDAO canRegDAO) {
         this.newVersion = newVersion;
-        this.canRegDAO=canRegDAO;
+        this.canRegDAO = canRegDAO;
     }
 
     /**
@@ -31,11 +33,16 @@ public class Migrator {
     public void migrate() {
         String databaseVersion = canRegDAO.getSystemPropery("DATABASE_VERSION");
 
-        if (databaseVersion==null) databaseVersion = "4.99.0";
+        if (databaseVersion == null) {
+            databaseVersion = "4.99.0";
+        }
 
         if (!databaseVersion.equalsIgnoreCase(newVersion)) {
             if (databaseVersion.compareTo("4.99.5") < 0) {
                 migrateTo_4_99_5(canRegDAO);
+            }
+            if (databaseVersion.compareTo("5.00.06") < 0) {
+                migrateTo_5_00_06(canRegDAO);
             }
         }
         canRegDAO.setSystemPropery("DATABASE_VERSION", newVersion);
@@ -46,5 +53,15 @@ public class Migrator {
         db.dropAndRebuildUsersTable();
         db.setSystemPropery("DATABASE_VERSION", "4.99.5");
         Logger.getLogger(Migrator.class.getName()).log(Level.INFO, "Migrated the database to version 4.99.5.");
+    }
+
+    private void migrateTo_5_00_06(CanRegDAO db) {
+        try {
+            db.dropAndRebuildKeys();
+            db.setSystemPropery("DATABASE_VERSION", "5.00.06");
+            Logger.getLogger(Migrator.class.getName()).log(Level.INFO, "Migrated the database to version 5.00.06.");
+        } catch (SQLException ex) {
+            Logger.getLogger(Migrator.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
