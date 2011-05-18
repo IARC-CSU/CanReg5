8e8ec690ced0c1d1bee500cd77f3777571ed4ae1
Morten Johannes Ervik <ervikm@iarc.fr>
2011-05-18 14:55:39 +0200

    Updated dummy R program.

--- conf/tables/r/test.r
+++ conf/tables/r/test.r
@@ -1,11 +1,32 @@
+# 1st argument is either pdf, png, ps or svg
+# 2nd argument is report file name - or output file name if you want
+# 3rd argument is population file name
+# 4th argument is incidence file name
+
 Args <- commandArgs(TRUE)
 # garbage <- dev.off()
-png(file=Args[1], bg="transparent")
+
+if (Args[1] == "png") {
+    filename <- paste( Args[2], ".png", sep = "" )
+    png(file=filename, bg="transparent")
+} else if (Args[1] == "pdf") { 
+    filename <- paste( Args[2], ".pdf" , sep = "")
+    pdf(file=filename) 
+} else { 
+    filename <- paste( Args[2], ".png" , sep = "")
+    png(file=filename, bg="transparent") 
+}
+
+# call proper function
+# in this test we call the rainbow wheel thingy stolen from the demo(graphics)
 par(bg = "gray")
 pie(rep(1,24), col = rainbow(24), radius = 0.9)
 dev.off()
-cat(Args[1])
-# cat("\n")
-# cat(Args[2])
+
+# write the name of any file created by R to out
+cat(filename)
+
 # cat("\n")
 # cat(Args[3])
+# cat("\n")
+# cat(Args[4])
