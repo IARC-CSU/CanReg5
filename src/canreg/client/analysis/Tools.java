a448ac09bc69fb4f5bdd262fdff546f19d498815
Morten Johannes Ervik <ervikm@iarc.fr>
2011-05-20 17:53:19 +0200

    Added SVG writer.

--- src/canreg/client/analysis/Tools.java
+++ src/canreg/client/analysis/Tools.java
@@ -6,20 +6,33 @@ package canreg.client.analysis;
 
 import canreg.common.database.PopulationDataset;
 import canreg.common.database.PopulationDatasetsEntry;
+import java.awt.Rectangle;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.LinkedList;
 import java.util.Map;
+import org.apache.batik.dom.GenericDOMImplementation;
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.jfree.chart.JFreeChart;
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
 
 /**
  *
  * @author ervikm
  */
 public class Tools {
+
     /**
      * 
      */
     public static int DONT_COUNT = -999;
+
     /**
      * 
      */
@@ -78,7 +91,7 @@ public class Tools {
          */
         numberOfCancerGroups
     }
-    
+
     /**
      * 
      * @param popoutput
@@ -89,16 +102,18 @@ public class Tools {
      */
     public static void writePopulationsToFile(BufferedWriter popoutput, int startYear, PopulationDataset[] populations, String separator) throws IOException {
         String popheader = "YEAR" + separator;
-        popheader += "SEX" + separator;
+        popheader += "AGE_GROUP_LABEL" + separator;
         popheader += "AGE_GROUP" + separator;
+        popheader += "SEX" + separator;
         popheader += "COUNT";
         popoutput.append(popheader);
         popoutput.newLine();
         int thisYear = startYear;
         for (PopulationDataset popset : populations) {
+            String[] ageGroupNames = popset.getAgeGroupStructure().getAgeGroupNames(); 
             for (PopulationDatasetsEntry pop : popset.getAgeGroups()) {
-                popoutput.append(thisYear + "");
-                popoutput.append(separator);
+                popoutput.append(thisYear + "").append(separator);
+                popoutput.append(ageGroupNames[pop.getAgeGroup()]).append(separator);
                 popoutput.append(pop.getStringRepresentationOfAgeGroupsForFile(separator));
                 popoutput.newLine();
             }
@@ -118,7 +133,7 @@ public class Tools {
     public static int assignICDGroupIndex(Map<KeyGroupsEnum, Integer> keyGroupsMap, String icdString, String morphologyString, LinkedList[] cancerGroupsLocal) {
         int icdIndex = -1;
         int icdNumber = -1;
-        
+
         // try first only with morphology
         if (morphologyString.length() > 0) {
             int morphology = Integer.parseInt(morphologyString);
@@ -130,7 +145,7 @@ public class Tools {
         }
 
         // Not found only with morphology        
-        if (icdIndex < 0) {  
+        if (icdIndex < 0) {
             if (icdString.length() > 0
                     && icdString.trim().substring(0, 1).equals("C")) {
                 icdString = icdString.trim().substring(1);
@@ -172,5 +187,33 @@ public class Tools {
             }
         }
         return icdIndex;
+    }
+
+    /**
+     * Exports a JFreeChart to a SVG file.
+     * 
+     * @param chart JFreeChart to export
+     * @param bounds the dimensions of the viewport
+     * @param svgFile the output file.
+     * @throws IOException if writing the svgFile fails.
+     */
+    public static void exportChartAsSVG(JFreeChart chart, Rectangle bounds, File svgFile) throws IOException {
+        // Get a DOMImplementation and create an XML document
+        DOMImplementation domImpl =
+                GenericDOMImplementation.getDOMImplementation();
+        Document document = domImpl.createDocument(null, "svg", null);
+
+        // Create an instance of the SVG Generator
+        SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+
+        // draw the chart in the SVG generator
+        chart.draw(svgGenerator, bounds);
+
+        // Write svg file
+        OutputStream outputStream = new FileOutputStream(svgFile);
+        Writer out = new OutputStreamWriter(outputStream, "UTF-8");
+        svgGenerator.stream(out, true /* use css */);
+        outputStream.flush();
+        outputStream.close();
     }
 }
