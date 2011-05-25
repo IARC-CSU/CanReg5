201bfba2da3839b6676247f346a416eadff816ff
Morten Johannes Ervik <ervikm@iarc.fr>
2011-05-25 16:10:46 +0200

    Implemented an exception to handle incompatible population sets.

---
+++ src/canreg/common/database/IncompatiblePopulationDataSetException.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package canreg.common.database;
+
+/**
+ *
+ * @author ErvikM
+ */
+public class IncompatiblePopulationDataSetException extends Exception {
+    
+}
