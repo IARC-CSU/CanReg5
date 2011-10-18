6baf2c7f061f159a0da38705c25f820c8751af13
Morten Johannes Ervik <ervikm@iarc.fr>
2011-10-18 15:49:11 +0200

    Added the empty TRN database to the version control. Added the CanReg.exe to the version control. Created an installer script. Added the generation of the installer and the .zip file to the ant build script.

---
+++ create-zip.bat
@@ -0,0 +1,15 @@
+set zippath="C:\Program Files\7-Zip"
+set zx=%zippath%\7z.exe
+set distfolder=dist
+REM if not exist %isx% set errormsg=%isx% not found && goto errorhandler
+%zx% a -tzip CanReg5.zip conf
+%zx% a -tzip CanReg5.zip demo
+%zx% a -tzip CanReg5.zip CanReg.exe
+copy doc\CanReg5-Instructions\CanReg5-Instructions.pdf doc\
+%zx% a -tzip CanReg5.zip doc\CanReg5-Instructions.pdf
+%zx% a -tzip CanReg5.zip changelog.txt
+cd %distfolder%
+%zx% a -tzip ..\CanReg5.zip .
+move ..\CanReg5.zip .
+cd ..
+goto :eof
---
+++ inno-build.bat
@@ -0,0 +1,6 @@
+set isxpath="C:\Program Files (x86)\Inno Setup 5"
+set isx=%isxpath%\iscc.exe
+set iwz=CanReg5-InnoSetup.iss
+REM if not exist %isx% set errormsg=%isx% not found && goto errorhandler
+%isx% "%iwz%"
+goto :eof
