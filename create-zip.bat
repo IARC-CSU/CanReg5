95dbc66123f34ded0d4b0c3dee902d1d7cacad02
Morten Johannes Ervik <ervikm@iarc.fr>
2012-05-18 16:52:54 +0200

    Updated the zipper batch file to include the scripts.

--- create-zip.bat
+++ create-zip.bat
@@ -4,6 +4,7 @@ set distfolder=dist
 REM if not exist %isx% set errormsg=%isx% not found && goto errorhandler
 %zx% a -tzip CanReg5.zip conf
 %zx% a -tzip CanReg5.zip demo
+%zx% a -tzip CanReg5.zip scripts
 %zx% a -tzip CanReg5.zip CanReg.exe
 copy doc\CanReg5-Instructions\CanReg5-Instructions.pdf doc\
 %zx% a -tzip CanReg5.zip doc\CanReg5-Instructions.pdf
