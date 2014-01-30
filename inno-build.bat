144eba8e14f5020df73c511b7790fdc6768254a0
Morten Johannes Ervik <ervikm@iarc.fr>
2014-01-30 15:56:03 +0100

    Added Scripts to distribute R packages for CanReg5.

--- inno-build.bat
+++ inno-build.bat
@@ -1,11 +1,15 @@
 set isxpath="C:\Program Files (x86)\Inno Setup 5"
 set isx=%isxpath%\iscc.exe
 set iwz=CanReg5-InnoSetup.iss
+set iwz-setup=CanReg5-R-packages-InnoSetup.iss
 set zippath="C:\Program Files\7-Zip"
 set zx=%zippath%\7z.exe
 REM if not exist %isx% set errormsg=%isx% not found && goto errorhandler
 %isx% "%iwz%"
+%isx% "%iwz-setup%"
 cd dist
 %zx% a -tzip CanReg5-Setup.zip CanReg5-Setup.exe
+%zx% a -tzip CanReg5-R-Packages-Setup.zip CanReg5-R-Packages-Setup.exe
 move CanReg5-Setup.zip web\
+move CanReg5-R-Packages-Setup.zip web\
 goto :eof
