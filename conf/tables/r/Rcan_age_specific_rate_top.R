9dd242d33aafaa316b0224d334f8ce5ee63d0f61
Morten Johannes Ervik <ervikm@iarc.fr>
2017-02-09 12:01:22 +0100

    Testing the Cairo library to generate PDFs better suited for non-Latin character-sets.

--- conf/tables/r/Rcan_age_specific_rate_top.R
+++ conf/tables/r/Rcan_age_specific_rate_top.R
@@ -11,7 +11,7 @@
   ################
 
   ## install packages missing and require them
-  invisible(canreg_load_packages(c("Rcpp", "data.table", "ggplot2", "gridExtra", "scales")))
+  invisible(canreg_load_packages(c("Rcpp", "data.table", "ggplot2", "gridExtra", "scales", "Cairo")))
 	
   
   ## get Args from canreg
