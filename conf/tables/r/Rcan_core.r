66437a4e4eae3d0d9a9af6f9fe69b999817d14ee
Morten Johannes Ervik <ervikm@iarc.fr>
2017-02-09 12:01:22 +0100

    Testing the Cairo library to generate PDFs better suited for non-Latin character-sets.

--- conf/tables/r/Rcan_core.r
+++ conf/tables/r/Rcan_core.r
@@ -1372,9 +1372,9 @@ canreg_output <- function(output_type="pdf",filename=NULL, landscape = FALSE,lis
   tiff_height <- ifelse(landscape, 2480 , 3508 )
   svg_width <- ifelse(landscape, 11.692 , 8.267 )
   svg_height <- ifelse(landscape, 8.267 , 11.692 )
-  pdf_width <- ifelse(landscape, 11.692 , 8.267 )    # Needs tuning
-  pdf_height <- ifelse(landscape, 8.267 , 11.692 )   # Needs tuning
-  
+  pdf_width <- ifelse(landscape, 11.692 , 8.267 )    # Needs tuning of margins
+  pdf_height <- ifelse(landscape, 8.267 , 11.692 )   # Needs tuning of margins
+
   file_number <- ifelse(list_graph, "%03d", "")
   
   
