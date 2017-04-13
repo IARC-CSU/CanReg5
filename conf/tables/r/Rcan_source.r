75c0e382758e1b7aedd4c33b41194fb0186d588a
Mathieu Laversanne <timat35@gmail.com>
2017-04-13 12:20:42 +0200

    font size and report lyout
    change pie and age-specific chart font size and on the report put
    ASChart side by side

--- conf/tables/r/Rcan_source.r
+++ conf/tables/r/Rcan_source.r
@@ -3,7 +3,7 @@
 
 ## install packages missing and require them
 
-   invisible(canreg_load_packages(c("Rcpp", "data.table", "ggplot2", "gridExtra", "scales", "Cairo","grid","ReporteRs")))
+  invisible(canreg_load_packages(c("Rcpp", "data.table", "ggplot2", "gridExtra", "scales", "Cairo","grid","ReporteRs")))
 
   ## get Args from canreg  
   skin <- FALSE
