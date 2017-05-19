b3592920c603b73aee88c8da35f0c2ced57d1ca3
Morten Johannes Ervik <ervikm@iarc.fr>
2017-05-19 15:37:40 +0200

    Initial RSS feed generated based on twitter archive and changelog.

--- scripts/rssfeed/initial-rss-feed-generator.rb
+++ scripts/rssfeed/initial-rss-feed-generator.rb
@@ -1,8 +1,28 @@
 require "rss"
+require "csv"
 
-changelog_file = "changelog.txt"
+changelog_file    = "../../changelog.txt"
+twitter_feed_file = "./tweets.csv"
 link = "http://www.iacr.com.fr/CanReg5"
+rss_file = "rss.xml"
 changelog = []
+
+versionToDateMap = {}
+
+CSV.open(twitter_feed_file, headers: true ).each do |line|
+  version = nil
+  if /CanReg5 version (5\.\d\d.\d\d)/=~line["text"]
+    version = $1    
+  elsif /CanReg5 update (\d\d)/=~line["text"]
+    version = "5.00.#{$1}"
+  end
+  unless version.nil?
+    date = Time.parse(line["timestamp"])
+    versionToDateMap[version] = date
+  end
+end
+puts versionToDateMap
+# exit
 
 mode = :start
 version = nil
@@ -10,13 +30,13 @@ text = []
 File.read(changelog_file).each_line do |line|
   if line=~/(\d\.\d\d\.\d\d)/
     changelog << { 
-      title: version, 
+      version: version, 
       summary: text.join("\n"),
       link: link,
-
       } unless version.nil?
     mode = :read
     version = $1
+    text = []
   else
     if mode == :start
       # skip
@@ -25,6 +45,9 @@ File.read(changelog_file).each_line do |line|
     end
   end
 end
+
+changelog.sort_by! {|v| v[:version]}
+
 rss = RSS::Maker.make("atom") do |maker|
   maker.channel.author = "Morten Ervik"
   maker.channel.updated = Time.now.to_s
@@ -32,15 +55,20 @@ rss = RSS::Maker.make("atom") do |maker|
   maker.channel.title = "CanReg5 Updates"
 
   changelog.each do |p|
+    unless versionToDateMap[p[:version]].nil?
       maker.items.new_item do |item|
           item.link    = p[:link]
-          item.title   = p[:title]
-          # item.updated = p.edited
-          # item.pubDate = p.date
+          item.title   = p[:version]
+          item.updated = versionToDateMap[p[:version]].to_s
+          item.pubDate = versionToDateMap[p[:version]].to_s
           item.summary = p[:summary]
           # item.content.content = 'text to set as content'
       end
+    end
   end
 end
 
 puts rss
+
+File.open(rss_file, "w") << rss
+
