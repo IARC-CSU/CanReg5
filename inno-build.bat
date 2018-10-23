380461e28a6c160ccaee6156c9de7f9ec7c4ea65
Morten Johannes Ervik <ervikm@iarc.fr>
2018-10-23 14:45:56 +0200

    Moved the InnoSetup scripts

--- inno-build.bat
+++ inno-build.bat
@@ -1,8 +1,8 @@
 set isxpath="C:\Program Files (x86)\Inno Setup 5"
 set isx=%isxpath%\iscc.exe
-set iwz=CanReg5-InnoSetup.iss
-set iwz-r=CanReg5-R-packages-InnoSetup.iss
-set iwz-w-r=CanReg5-with-R-packages-InnoSetup.iss
+set iwz=scripts\InnoSetup\CanReg5-InnoSetup.iss
+set iwz-r=scripts\InnoSetup\CanReg5-R-packages-InnoSetup.iss
+set iwz-w-r=scripts\InnoSetup\CanReg5-with-R-packages-InnoSetup.iss
 set zippath="C:\Program Files\7-Zip"
 set zx=%zippath%\7z.exe
 REM if not exist %isx% set errormsg=%isx% not found && goto errorhandler
