9e715a0870ff9976e56d22296fabf3b99c0c35df
laversanne <timat35@gmail.com>
2019-05-02 15:40:58 +0200

    sds

--- conf/tables/r/Rcan_shiny_table.r
+++ conf/tables/r/Rcan_shiny_table.r
@@ -15,7 +15,7 @@
   
 tryCatch({
   
-  canreg_load_packages(c("Rcpp", "data.table", "ggplot2","shiny","shinydashboard", "shinyjs","gridExtra", "scales", "Cairo","grid","officer","flextable", "zip", "bmp", "jpeg", "png","shiny.i18n", "Rcan"))
+  canreg_load_packages(c("Rcpp", "data.table", "ggplot2","shiny","shinydashboard", "shinyjs","shinyFiles","gridExtra", "scales", "Cairo","grid","officer","flextable", "zip", "bmp", "jpeg", "png","shiny.i18n", "Rcan"))
 	i18n <- Translator(translation_csvs_path  = (paste(sep="/", script.basename, "r-translations")))
 	i18n$set_translation_language(ls_args$lang)
   
@@ -33,7 +33,7 @@ tryCatch({
   dt_CI5_list <- readRDS(paste0(script.basename, "/CI5_alldata.rds"))
   dt_CI5_label <- as.character(unique(dt_CI5_list[cr == ls_args$sr, c("country_label"), with=FALSE])$country_label)
 
-  
+  volumes <- c(Home = getVolumes()(),fs::path_home())
   shiny_dir <- paste(sep="/", script.basename, "shiny")
   runApp(appDir =shiny_dir, launch.browser =TRUE)
  
