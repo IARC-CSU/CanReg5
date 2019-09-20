589ce2f3500ed8a084e443b94ae45a02093dcbff
Morten Ervik <ervikm@iarc.fr>
2019-09-20 15:22:26 +0200

    ICD-O-3 to ICD10 and ICCC3 jruby scripts added.

---
+++ scripts/icd10_iccc_converter/install.bat
@@ -0,0 +1,3 @@
+REM setx JRUBY_OPTS --1.9
+jgem install bundler
+jruby --1.9 -S bundle install
---
+++ scripts/icd10_iccc_converter/run.bat
@@ -0,0 +1,1 @@
+jruby --1.9 icd10_iccc_converter.rb -t TOP -m MOR -b BEH -s SEX -g -f "Here be Data.csv"
