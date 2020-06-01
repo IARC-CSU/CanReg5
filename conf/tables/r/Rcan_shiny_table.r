f98ff1047bfd654bb4401526875fa5bde3371f56
laversanne <timat35@gmail.com>
2020-06-01 22:33:27 +0200

    Rcan last version + not sure what it is...

--- conf/tables/r/Rcan_shiny_table.r
+++ conf/tables/r/Rcan_shiny_table.r
@@ -4,32 +4,15 @@
   script.name <- sub(file.arg.name, "", 
                      initial.options[grep(file.arg.name, initial.options)])
   script.basename <- dirname(script.name)
-  
-  ## Load Rcan function
   source(paste(sep="/", script.basename, "Rcan_core.r"))
+
   
   ## to get canreg argument list
   Args <- commandArgs(TRUE)
-  ls_args <- canreg_args(Args)
-  
   
 tryCatch({
-  
-  canreg_load_packages(c("Rcpp", "data.table", "ggplot2","shiny","shinydashboard", "shinyjs","gridExtra", "scales", "Cairo","grid","officer","flextable", "zip", "bmp", "jpeg", "png","shiny.i18n", "Rcan"))
-	i18n <- Translator(translation_csvs_path  = (paste(sep="/", script.basename, "r-translations")))
-	i18n$set_translation_language(ls_args$lang)
-  
-  dt_base <- csu_merge_inc_pop(
-    inc_file =ls_args$inc,
-    pop_file =ls_args$pop,
-    group_by = c("ICD10GROUP", "ICD10GROUPLABEL","ICD10GROUPCOLOR", "YEAR", "SEX"),
-    column_group_list =list(c("ICD10GROUP", "ICD10GROUPLABEL", "ICD10GROUPCOLOR"))
-  )
-  
-  
-  canreg_age_group <- canreg_get_agegroup_label(dt_base, ls_args$agegroup)
-  year_info <- canreg_get_years(dt_base)
-  
+
+  canreg_load_packages(c("data.table", "ggplot2","shiny","shinydashboard", "shinyjs","gridExtra", "scales", "Cairo","officer","flextable", "zip", "bmp", "jpeg", "png","shiny.i18n", "Rcan"))
   shiny_dir <- paste(sep="/", script.basename, "shiny")
   runApp(appDir =shiny_dir, launch.browser =TRUE)
  
@@ -45,6 +28,7 @@ tryCatch({
       print(doc, ls_args$filename)
      if (file.exists(ls_args$filename)) file.remove(ls_args$filename)
     }
+
     
     canreg_error_log(e,ls_args$filename,ls_args$out,Args,ls_args$inc,ls_args$pop)
   }
