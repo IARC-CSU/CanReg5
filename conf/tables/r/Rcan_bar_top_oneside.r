d8aaafe70edb17cd329a96654f511d5f0e015a96
Mathieu Laversanne <timat35@gmail.com>
2020-10-19 15:29:18 +0200

    fix shiny.18n update

--- conf/tables/r/Rcan_bar_top_oneside.r
+++ conf/tables/r/Rcan_bar_top_oneside.r
@@ -17,7 +17,7 @@ tryCatch({
   
   #load dependency packages
   canreg_load_packages(c("data.table", "ggplot2", "gridExtra", "scales", "Cairo","bmp", "jpeg",  "shiny.i18n", "Rcan"))
- 	i18n <- Translator(translation_csvs_path  = (paste(sep="/", script.basename, "r-translations")))
+ 	i18n <- Translator$new(translation_csvs_path  = (paste(sep="/", script.basename, "r-translations")))
 	i18n$set_translation_language(ls_args$lang)
 	
   #merge incidence and population
