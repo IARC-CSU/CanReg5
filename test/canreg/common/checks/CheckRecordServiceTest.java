77cca78b684a7184e0074ad9330b3449af779310
sminfotel <stephane.maheo@infotel.com>
2022-02-15 16:31:58 +0100

    Updated .gitignore.

--- test/canreg/common/checks/CheckRecordServiceTest.java
+++ test/canreg/common/checks/CheckRecordServiceTest.java
@@ -4,6 +4,8 @@ import canreg.client.LocalSettings;
 import canreg.common.Tools;
 import canreg.common.database.Dictionary;
 import canreg.common.database.Patient;
+import canreg.common.database.Source;
+import canreg.common.database.Tumour;
 import canreg.server.database.CanRegDAO;
 import canreg.server.management.SystemDescription;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -38,14 +40,14 @@ public class CheckRecordServiceTest {
 
     private AutoCloseable closeable;
 
-    @Before 
+    @Before
     public void before() throws IOException {
         closeable = MockitoAnnotations.openMocks(this);
-        
+
         systemDescription = buildSystemDescription(xmlRegistryFile);
         Mockito.when(dao.getDatabaseVariablesList()).thenReturn(systemDescription.getDatabaseVariableListElements());
         service = new CheckRecordService(dao);
-        
+
         Map<Integer, Dictionary> dictionariesMap = CanRegDAO.buildDictionaryMap(systemDescription.getSystemDescriptionDocument());
         service.setDictionaries(dictionariesMap);
 
@@ -53,11 +55,12 @@ public class CheckRecordServiceTest {
         InputStream inputStream = FileUtils.openInputStream(dictionariesFile);
         Map<String, ListDictionaryEntries> dictionaryEntries = new ObjectMapper().readerForMapOf(ListDictionaryEntries.class).readValue(inputStream);
         Assert.assertNotNull(dictionaryEntries);
-        for(Map.Entry<String, ListDictionaryEntries> entry : dictionaryEntries.entrySet()) {
+        for (Map.Entry<String, ListDictionaryEntries> entry : dictionaryEntries.entrySet()) {
             dictionariesMap.get(Integer.parseInt(entry.getKey())).getDictionaryEntries().putAll(entry.getValue().getDictionaryEntries());
         }
-        
+
     }
+
     @After
     public void releaseMocks() throws Exception {
         closeable.close();
@@ -66,21 +69,59 @@ public class CheckRecordServiceTest {
     @Test
     public void testCheckPatientOk() {
         Patient patient = new Patient();
-        patient.setVariable("regno","123");
+        patient.setVariable("regno", "123");
         patient.setVariable("famn", "FamilyName");
         patient.setVariable("birthd", "01/01/1910");
-        patient.setVariable("sex","1");
+        patient.setVariable("sex", "1");
         List<String> messages = service.checkPatient(patient);
+        Assert.assertEquals("[]", messages.toString());
+    }
+
+    @Test
+    public void testCheckTumourOk() {
+        Tumour tumour = new Tumour();
+        tumour.setVariable("incid", "20010202");
+        tumour.setVariable("beh", "3");
+        tumour.setVariable("mor", "8211");
+        tumour.setVariable("top", "189");
+        tumour.setVariable("bas", "7");
+        tumour.setVariable("recs", "1");
+        tumour.setVariable("addr", "74000");
+        tumour.setVariable("age", 89);
+        List<String> messages = service.checkTumour(tumour);
+        Assert.assertEquals("[]", messages.toString());
+    }
+
+    @Test
+    public void testCheckTumourAgeNotNumber() {
+        Tumour tumour = new Tumour();
+        tumour.setVariable("incid", "20010202");
+        tumour.setVariable("beh", "3");
+        tumour.setVariable("mor", "8211");
+        tumour.setVariable("top", "189");
+        tumour.setVariable("bas", "7");
+        tumour.setVariable("recs", "1");
+        tumour.setVariable("addr", "74000");
+        // Age is not a number object
+        tumour.setVariable("age", "89");
+        List<String> messages = service.checkTumour(tumour);
+        Assert.assertEquals("[integer input is expected for variable: age]", messages.toString());
+    }
+    
+    @Test
+    public void testCheckSourceOk() {
+        Source source = new Source();
+        List<String> messages = service.checkSource(source);
         Assert.assertEquals("[]", messages.toString());
     }
 
     @Test
     public void testCheckRecordMissingVariable() {
         Patient patient = new Patient();
-        patient.setVariable("regno","123");
+        patient.setVariable("regno", "124");
         // patient.setVariable("famn", "FamilyName");
         patient.setVariable("birthd", "01/01/1910");
-        patient.setVariable("sex","1");
+        patient.setVariable("sex", "2");
         List<String> messages = service.checkPatient(patient);
         Assert.assertEquals("[variable is mandatory: famn]", messages.toString());
     }
@@ -88,10 +129,10 @@ public class CheckRecordServiceTest {
     @Test
     public void testCheckRecordEmptyVariable() {
         Patient patient = new Patient();
-        patient.setVariable("regno","123");
+        patient.setVariable("regno", "125");
         patient.setVariable("famn", "");
         patient.setVariable("birthd", "01/01/1910");
-        patient.setVariable("sex","1");
+        patient.setVariable("sex", "1");
         List<String> messages = service.checkPatient(patient);
         Assert.assertEquals("[variable is mandatory: famn]", messages.toString());
     }
@@ -99,10 +140,10 @@ public class CheckRecordServiceTest {
     @Test
     public void testCheckRecordUnknownValueInDictionary() {
         Patient patient = new Patient();
-        patient.setVariable("regno","123");
+        patient.setVariable("regno", "126");
         patient.setVariable("famn", "FamilyName");
         patient.setVariable("birthd", "01/01/1910");
-        patient.setVariable("sex","3");
+        patient.setVariable("sex", "3");
         List<String> messages = service.checkPatient(patient);
         Assert.assertEquals("[unknown value in dictionary sex=3]", messages.toString());
     }
@@ -110,11 +151,11 @@ public class CheckRecordServiceTest {
     @Test
     public void testCheckRecordUnknownVariable() {
         Patient patient = new Patient();
-        patient.setVariable("regno","123");
+        patient.setVariable("regno", "123");
         patient.setVariable("famn", "FamilyName");
         patient.setVariable("birthd", "01/01/1910");
-        patient.setVariable("sex","2");
-        patient.setVariable("unknownVariable","3");
+        patient.setVariable("sex", "2");
+        patient.setVariable("unknownVariable", "3");
         List<String> messages = service.checkPatient(patient);
         Assert.assertEquals("[unknown variable: unknownvariable]", messages.toString());
     }
